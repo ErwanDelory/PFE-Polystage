e2edde86fd657a649b8b38ea15bf6b4a192f0cd1
Dordb <43724442+Dordb@users.noreply.github.com+5915265+dorx48@gmail.com>
2020-12-06 22:30:12 +0100

    add qq routes :)

--- Node/api/controllers/userController.js
+++ Node/api/controllers/userController.js
@@ -1,35 +1,95 @@
 const db = require("../../mysqlConnect");
 const config = require("../../config");
 
-async function getEleves(req, res, next) {
+function success() {}
+
+function getEleves(req, res, next) {
 	let query = `SELECT ${config.table.utilisateur.id},${config.table.utilisateur.nom},${config.table.utilisateur.prenom},${config.table.utilisateur.email},${config.table.infoetu.numetudiant},${config.table.infoetu.niveau},${config.table.infoetu.annee}
     from ${config.table.utilisateur.tablename} LEFT JOIN ${config.table.infoetu.tablename} ON ${config.table.utilisateur.id} = ${config.table.infoetu.id}
     WHERE ${config.table.utilisateur.role} = "Etudiant"`;
 
 	db.query(query, (err, result) => {
 		if (err) throw err;
-		res.status(200).json({ message: " Add Ok .", data: result });
+		res.status(200).json({ message: "Ok .", data: result });
+	});
+}
+function getEleveById(req, res, next) {
+	let query = `SELECT ${config.table.utilisateur.id},${config.table.utilisateur.nom},${config.table.utilisateur.prenom},${config.table.utilisateur.email},${config.table.infoetu.numetudiant},${config.table.infoetu.niveau},${config.table.infoetu.annee}
+    from ${config.table.utilisateur.tablename} LEFT JOIN ${config.table.infoetu.tablename} ON ${config.table.utilisateur.id} = ${config.table.infoetu.id}
+    WHERE ${config.table.utilisateur.id}=${req.params.id} AND ${config.table.utilisateur.role} = "Etudiant"`;
+
+	db.query(query, (err, result) => {
+		if (err) throw err;
+		res.status(200).json({ message: "Ok .", data: result });
 	});
 }
 
-async function getEnseignants(req, res, next) {
+function getEnseignants(req, res, next) {
 	let query = `SELECT ${config.table.utilisateur.id},${config.table.utilisateur.nom},${config.table.utilisateur.prenom},${config.table.utilisateur.email} from ${config.table.utilisateur.tablename} WHERE ${config.table.utilisateur.role} = "Enseignant"`;
 
 	db.query(query, (err, result) => {
 		if (err) throw err;
-		res.status(200).json({ message: " Add Ok .", data: result });
+		res.status(200).json({ message: "Ok .", data: result });
 	});
 }
 
-async function getTuteurs(req, res, next) {
+function getTuteurs(req, res, next) {
 	let query = `SELECT ${config.table.utilisateur.id},${config.table.utilisateur.nom},${config.table.utilisateur.prenom},${config.table.utilisateur.email} from ${config.table.utilisateur.tablename} WHERE ${config.table.utilisateur.role} = "Tuteur"`;
 
 	db.query(query, (err, result) => {
 		if (err) throw err;
-		res.status(200).json({ message: " Add Ok .", data: result });
+		res.status(200).json({ message: "Ok .", data: result });
+	});
+}
+function getUsers(req, res, next) {
+	let query = `SELECT ${config.table.utilisateur.id},${config.table.utilisateur.nom},${config.table.utilisateur.prenom},${config.table.utilisateur.email}
+    from ${config.table.utilisateur.tablename}`;
+
+	db.query(query, (err, result) => {
+		if (err) throw err;
+		res.status(200).json({ message: "Ok .", data: result });
+	});
+}
+function getUserById(req, res, next) {
+	let query = `SELECT ${config.table.utilisateur.id},${config.table.utilisateur.nom},${config.table.utilisateur.prenom},${config.table.utilisateur.email}
+    from ${config.table.utilisateur.tablename}
+    WHERE ${config.table.utilisateur.id}=${req.params.id}`;
+
+	db.query(query, (err, result) => {
+		if (err) throw err;
+		res.status(200).json({ message: "Ok .", data: result });
+	});
+}
+
+function getStage(req, res, next) {
+	let query = `SELECT *
+	from ${config.table.stage.tablename}`;
+	db.query(query, (err, result) => {
+		if (err) throw err;
+		success(result);
+		res.status(200).json({ message: "Ok .", data: result });
+	});
+}
+
+function getStageById(req, res, next) {
+	let query = `SELECT *
+	from ${config.table.stage.tablename}
+	WHERE ${config.table.stage.idstage} = ${req.params.id}`;
+	db.query(query, (err, result) => {
+		if (err) throw err;
+		success(result);
+		res.status(200).json({ message: "Ok .", data: result });
 	});
 }
 
 exports.getEleves = getEleves;
+exports.getEleveById = getEleveById;
+
 exports.getEnseignants = getEnseignants;
 exports.getTuteurs = getTuteurs;
+
+exports.getUsers = getUsers;
+exports.getUserById = getUserById;
+
+exports.getStage = getStage;
+exports.getStageById = getStageById;
--- Node/api/routes/route.js
+++ Node/api/routes/route.js
@@ -4,11 +4,21 @@ const registerController = require("../controllers/registerController");
 const userControllers = require("../controllers/userController");
 
 const router = express.Router();
+
+//Authentification Controller
 router.post("/auth", authentificationControllers.auth);
 router.post("/register", registerController.register);
 
+//User Controller
 router.get("/eleves", userControllers.getEleves);
+router.get("/eleve/:id", userControllers.getEleveById);
+
 router.get("/enseignants", userControllers.getEnseignants);
 router.get("/tuteurs", userControllers.getTuteurs);
+router.get("/users", userControllers.getUsers);
+router.get("/user/:id", userControllers.getUserById);
+
+router.get("/stages", userControllers.getStage);
+router.get("/stage/:id", userControllers.getStageById);
 
 module.exports = router;
