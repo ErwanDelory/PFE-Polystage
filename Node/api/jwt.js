7216630fc17d1f6be83086e8102b3e47d448ef09
ErwanDelory <erwan.delory@gmail.com>
2020-12-09 22:21:20 +0100

    view & dl

--- Node/api/jwt.js
+++ Node/api/jwt.js
@@ -1,16 +1,17 @@
-const expressJwt = require("express-jwt");
-const config = require("./config.json");
+const expressJwt = require('express-jwt');
+const config = require('./config.json');
 
 module.exports = jwt;
 
 function jwt() {
-	const { secret } = config;
-	return expressJwt({ secret, algorithms: ["HS256"] }).unless({
-		path: [
-			// public routes that don't require authentication
-			"/api/auth",
-			"/api/register",
-			"/api/test",
-		],
-	});
+  const { secret } = config;
+  return expressJwt({ secret, algorithms: ['HS256'] }).unless({
+    path: [
+      // public routes that don't require authentication
+      '/api/auth',
+      '/api/register',
+      '/api/rapport/1',
+      '/api/dlrapport/1',
+    ],
+  });
 }
--- polystage-app/src/pages/home.jsx
+++ polystage-app/src/pages/home.jsx
@@ -1,118 +1,4 @@
-import React, { Component } from 'react';
-import { Button, Card, Col, Container, Row } from 'react-bootstrap';
-import img0 from './../img/stage-0.jpg';
-import img1 from './../img/stage-1.jpg';
-import img2 from './../img/stage-2.jpg';
-import img3 from './../img/stage-3.jpg';
-import img4 from './../img/stage-4.jpg';
-import img5 from './../img/stage-5.jpg';
-import img6 from './../img/stage-6.jpg';
-import img7 from './../img/stage-7.jpg';
-import img8 from './../img/stage-8.jpg';
-import img9 from './../img/stage-9.jpg';
-
-export default class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      token: sessionStorage.getItem('token'),
-      data: [],
-    };
-  }
-
-  componentDidMount() {
-    fetch('http://localhost:5000/api/stages', {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        Authorization: 'Bearer ' + this.state.token,
-      },
-    })
-      .then((res) => res.json())
-      .then((mes) => {
-        this.setState({ data: mes.data });
-      });
-  }
-
-  seeRapport(event) {
-    event.preventDefault();
-    const token = sessionStorage.getItem('token');
-    console.log(token);
-    fetch(`http://localhost:5000/api/rapport/1`, {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/pdf',
-        Authorization: 'Bearer ' + token,
-      },
-    }).then((res) => res.json());
-  }
-
-  render() {
-    const annee = [];
-    let img = [img0, img1, img2, img3, img4, img5, img6, img7, img8, img9];
-    const options = {
-      year: 'numeric',
-      month: 'numeric',
-      day: 'numeric',
-    };
-    for (let i = 0; i < this.state.data.length; i++) {
-      var a = new Date(this.state.data[i].datedebut).toLocaleDateString(
-        undefined,
-        options
-      );
-      var b = new Date(this.state.data[i].datefin).toLocaleDateString(
-        undefined,
-        options
-      );
-      annee.push(
-        <p>
-          {a} - {b}
-        </p>
-      );
-    }
-
-    return (
-      <div>
-        <Container>
-          <br />
-          <h3>Liste des stages</h3>
-          <Row xs={1} md={2}>
-            {this.state.data?.map((stage) => (
-              <Col>
-                <Card style={{ width: '35rem' }}>
-                  <Card.Img variant="top" src={img[stage.idstage - 1]} />
-                  <Card.Header className="text-center">
-                    Année {stage.annee} - {stage.niveau}A
-                  </Card.Header>
-                  <Card.Body>
-                    <Card.Title>{stage.titrestage}</Card.Title>
-                    <Card.Text>
-                      <small className="text-muted">{stage.entreprise}</small>
-                      <br />
-                      {stage.description}
-                    </Card.Text>
-                    <Button variant="warning" onClick={this.seeRapport}>
-                      Voir le rapport
-                    </Button>{' '}
-                    <Button variant="info" onClick={this.seeRapport}>
-                      Télécharger le rapport
-                    </Button>
-                  </Card.Body>
-                  <Card.Footer className="text-center">
-                    {annee[stage.idstage - 1]}
-                  </Card.Footer>
-                </Card>
-              </Col>
-            ))}
-          </Row>
-        </Container>
-      </div>
-    );
-  }
-}
-/*import React, { Component, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 import img0 from './../img/stage-0.jpg';
 import img1 from './../img/stage-1.jpg';
@@ -141,15 +27,67 @@ const Home = () => {
         return res.json();
       })
       .then((mes) => {
-        setData(mes.data);
-        console.log(data);
+        return setData(mes.data);
       });
   }, []);
 
+  const annee = [];
+  let img = [img0, img1, img2, img3, img4, img5, img6, img7, img8, img9];
+  const options = {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+  };
+  for (let i = 0; i < data.length; i++) {
+    var a = new Date(data[i].datedebut).toLocaleDateString(undefined, options);
+    var b = new Date(data[i].datefin).toLocaleDateString(undefined, options);
+    annee.push(
+      <p>
+        {a} - {b}
+      </p>
+    );
+  }
+
   return (
     <div>
-      <p>Salut</p>
+      <Container>
+        <br />
+        <h3>Liste des stages</h3>
+        <Row xs={1} md={2}>
+          {data?.map((stage) => (
+            <Col>
+              <Card style={{ width: '35rem' }}>
+                <Card.Img variant="top" src={img[stage.idstage - 1]} />
+                <Card.Header className="text-center">
+                  Année {stage.annee} - {stage.niveau}A
+                </Card.Header>
+                <Card.Body>
+                  <Card.Title>{stage.titrestage}</Card.Title>
+                  <Card.Text>
+                    <small className="text-muted">{stage.entreprise}</small>
+                    <br />
+                    {stage.description}
+                  </Card.Text>
+                  <a
+                    href="http://localhost:5000/api/rapport/1"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Button variant="warning">Voir le rapport</Button>
+                  </a>{' '}
+                  <a href="http://localhost:5000/api/dlrapport/1">
+                    <Button variant="info">Télécharger le rapport</Button>
+                  </a>
+                </Card.Body>
+                <Card.Footer className="text-center">
+                  {annee[stage.idstage - 1]}
+                </Card.Footer>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 };
-export default Home;*/
+export default Home;
