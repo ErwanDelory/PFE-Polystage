d8387c349269140d7a3fbe9e110a8a2015875126
ErwanDelory <erwan.delory@gmail.com>
2021-01-11 18:11:59 +0100

    add admin page

--- polystage-app/src/pages/admin.jsx
+++ polystage-app/src/pages/admin.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Button, Card, Container } from 'react-bootstrap';
 
 const Admin = () => {
   return (
     <div>
-      <p>Page admin Salut</p>
+      <Container>
+        <h3>Administration</h3>
+        <Card className="center" style={{ width: '70rem' }}>
+          <Card.Header>Gestion des retards</Card.Header>
+          <Card.Body>
+            <Card.Title>Tuteurs</Card.Title>
+            <Card.Text>
+              Nom et prénom du tuteur
+              <span className="warning">Mail d'avertissement déjà envoyé</span>
+              <span className="warning">Évaluation à faire</span>
+            </Card.Text>
+            <Card.Title>Élèves</Card.Title>
+            <Card.Text>
+              Nom et prénom de l'élève
+              <span className="warning">Mail d'avertissement déjà envoyé</span>
+              <span className="warning">Rapport à télécharger</span>
+              <span className="warning">Présentation à télécharger</span>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        <Button variant="warning">Envoyer les mails de rappel</Button>
+      </Container>
     </div>
   );
 };
