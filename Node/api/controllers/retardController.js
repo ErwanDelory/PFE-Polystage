4c2fa51a1591f8be82e27fbed4b4ad5c64852307
ErwanDelory <erwan.delory@gmail.com>
2021-01-14 23:16:55 +0100

    page admin done

--- Node/api/controllers/retardController.js
+++ Node/api/controllers/retardController.js
@@ -1,90 +1,90 @@
-const db = require("../../mysqlConnect");
-const config = require("../../config");
+const db = require('../../mysqlConnect');
+const config = require('../../config');
 
 function getRetardEleve(req, res, next) {
-	let query = `SELECT *
-    from ${config.table.retardeleve.tablename}`;
-	db.query(query, (err, result) => {
-		if (err) throw err;
-		res.status(200).json({ message: "Ok .", data: result });
-	});
+  let query = `SELECT *
+    from ${config.table.retardeleve.tablename} LEFT JOIN ${config.table.utilisateur.tablename} ON ${config.table.utilisateur.id} = ${config.table.retardeleve.id}`;
+  db.query(query, (err, result) => {
+    if (err) throw err;
+    res.status(200).json({ message: 'Ok .', data: result });
+  });
 }
 function newRetardEleve(req, res, next) {
-	let query = `INSERT INTO ${config.table.retardeleve.tablename}(iduti, mailenvoye, rapport, presentation, autoeval)
+  let query = `INSERT INTO ${config.table.retardeleve.tablename}(iduti, mailenvoye, rapport, presentation, autoeval)
     VALUES (${req.body.iduti},${req.body.mailenvoye},${req.body.rapport},${req.body.presentation},${req.body.autoeval})`;
-	db.query(query, (err, result) => {
-		if (err) {
-			if (err.code === "ER_DUP_ENTRY") {
-				return res.status(406).json({
-					message: "Retard deja créé",
-				});
-			} else {
-				throw err;
-			}
-		}
-		res.status(200).json({ message: "Ok .", data: result });
-	});
+  db.query(query, (err, result) => {
+    if (err) {
+      if (err.code === 'ER_DUP_ENTRY') {
+        return res.status(406).json({
+          message: 'Retard deja créé',
+        });
+      } else {
+        throw err;
+      }
+    }
+    res.status(200).json({ message: 'Ok .', data: result });
+  });
 }
 
 function deleteRetardEleve(req, res, next) {
-	let query = `DELETE FROM retardeleve WHERE retardeleve.iduti = ${req.params.iduti}`;
-	db.query(query, (err, result) => {
-		if (err) throw err;
+  let query = `DELETE FROM retardeleve WHERE retardeleve.iduti = ${req.params.iduti}`;
+  db.query(query, (err, result) => {
+    if (err) throw err;
 
-		res.status(200).json({ message: "Ok .", data: result });
-	});
+    res.status(200).json({ message: 'Ok .', data: result });
+  });
 }
 
 function updateRetardEleve(req, res, next) {
-	let query = `UPDATE retardeleve SET mailenvoye = ${req.body.mailenvoye}, rapport = ${req.body.rapport}, presentation = ${req.body.presentation}, autoeval = ${req.body.autoeval}  WHERE retardeleve.iduti = ${req.params.iduti};`;
-	db.query(query, (err, result) => {
-		if (err) throw err;
+  let query = `UPDATE retardeleve SET mailenvoye = ${req.body.mailenvoye}, rapport = ${req.body.rapport}, presentation = ${req.body.presentation}, autoeval = ${req.body.autoeval}  WHERE retardeleve.iduti = ${req.params.iduti};`;
+  db.query(query, (err, result) => {
+    if (err) throw err;
 
-		res.status(200).json({ message: "Ok .", data: result });
-	});
+    res.status(200).json({ message: 'Ok .', data: result });
+  });
 }
 
 function getRetardTuteur(req, res, next) {
-	let query = `SELECT *
-    from ${config.table.retardtuteur.tablename}`;
-	db.query(query, (err, result) => {
-		if (err) throw err;
-		res.status(200).json({ message: "Ok .", data: result });
-	});
+  let query = `SELECT *
+    from ${config.table.retardtuteur.tablename} LEFT JOIN ${config.table.utilisateur.tablename} ON ${config.table.utilisateur.id} = ${config.table.retardtuteur.id}`;
+  db.query(query, (err, result) => {
+    if (err) throw err;
+    res.status(200).json({ message: 'Ok .', data: result });
+  });
 }
 function newRetardTuteur(req, res, next) {
-	let query = `INSERT INTO retardtuteur (iduti, mailenvoye) 
+  let query = `INSERT INTO retardtuteur (iduti, mailenvoye) 
     VALUES ("${req.body.iduti}","${req.body.mailenvoye}")`;
-	db.query(query, (err, result) => {
-		if (err) {
-			if (err.code === "ER_DUP_ENTRY") {
-				return res.status(406).json({
-					message: "Retard deja créé",
-				});
-			} else {
-				throw err;
-			}
-		}
-		res.status(200).json({ message: "Ok .", data: result });
-	});
+  db.query(query, (err, result) => {
+    if (err) {
+      if (err.code === 'ER_DUP_ENTRY') {
+        return res.status(406).json({
+          message: 'Retard deja créé',
+        });
+      } else {
+        throw err;
+      }
+    }
+    res.status(200).json({ message: 'Ok .', data: result });
+  });
 }
 function deleteRetardTuteur(req, res, next) {
-	let query = `DELETE FROM retardtuteur WHERE retardtuteur.iduti = ${req.params.iduti}`;
-	db.query(query, (err, result) => {
-		if (err) throw err;
+  let query = `DELETE FROM retardtuteur WHERE retardtuteur.iduti = ${req.params.iduti}`;
+  db.query(query, (err, result) => {
+    if (err) throw err;
 
-		res.status(200).json({ message: "Ok .", data: result });
-	});
+    res.status(200).json({ message: 'Ok .', data: result });
+  });
 }
 
 function updateRetardTuteur(req, res, next) {
-	let query = `UPDATE retardtuteur SET mailenvoye = ${req.body.mailenvoye} 
+  let query = `UPDATE retardtuteur SET mailenvoye = ${req.body.mailenvoye} 
     WHERE retardtuteur.iduti = ${req.params.iduti};`;
-	db.query(query, (err, result) => {
-		if (err) throw err;
+  db.query(query, (err, result) => {
+    if (err) throw err;
 
-		res.status(200).json({ message: "Ok .", data: result });
-	});
+    res.status(200).json({ message: 'Ok .', data: result });
+  });
 }
 
 exports.getRetardEleve = getRetardEleve;
--- polystage-app/src/pages/admin.jsx
+++ polystage-app/src/pages/admin.jsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Card, Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const Admin = () => {
-  // TODO: Recherche des tuteurs en retard
-  // TODO: Recherche des élèves en retard
+  const [tuteur, setTuteur] = useState([]);
+  const [eleve, setEleve] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/retardtuteur', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + sessionStorage.getItem('token'),
+      },
+    })
+      .then((res) => res.json())
+      .then((mes) => {
+        return setTuteur(mes.data);
+      });
+
+    fetch('http://localhost:5000/api/retardeleve', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + sessionStorage.getItem('token'),
+      },
+    })
+      .then((res) => res.json())
+      .then((mes) => {
+        return setEleve(mes.data);
+      });
+  }, []);
 
   return (
     <div>
@@ -16,34 +44,65 @@ const Admin = () => {
           <Card.Header>Gestion des retards</Card.Header>
           <Card.Body>
             <Card.Title>Tuteurs</Card.Title>
-            <Card.Text>
-              Nom et prénom du tuteur
-              <span className="warning green">
-                Mail d'avertissement déjà envoyé&nbsp;
-                <FontAwesomeIcon className="bckg-icon" icon={faCheck} />
-              </span>
-              <span className="warning red">
-                Évaluation à faire&nbsp;
-                <FontAwesomeIcon className="bckg-icon" icon={faTimes} />
-              </span>
-            </Card.Text>
-            <Card.Text>Aucun tuteur n'est en retard !</Card.Text>
+
+            {tuteur.length !== 0 ? (
+              <div>
+                {tuteur?.map((data) => (
+                  <Card.Text key={data.id}>
+                    {data.prenom} {data.prenom}
+                    <span className="warning green">
+                      Mail d'avertissement déjà envoyé&nbsp;
+                      <FontAwesomeIcon className="bckg-icon" icon={faCheck} />
+                    </span>
+                    <span className="warning red">
+                      Évaluation à faire&nbsp;
+                      <FontAwesomeIcon className="bckg-icon" icon={faTimes} />
+                    </span>
+                  </Card.Text>
+                ))}
+              </div>
+            ) : (
+              <Card.Text>Aucun tuteur n'est en retard !</Card.Text>
+            )}
+
             <Card.Title>Élèves</Card.Title>
-            <Card.Text>
-              Nom et prénom de l'élève
-              <span className="warning green">
-                Mail d'avertissement déjà envoyé&nbsp;
-                <FontAwesomeIcon className="bckg-icon" icon={faCheck} />
-              </span>
-              <span className="warning red">
-                Rapport à télécharger&nbsp;
-                <FontAwesomeIcon className="bckg-icon" icon={faTimes} />
-              </span>
-              <span className="warning red">
-                Présentation à télécharger&nbsp;
-                <FontAwesomeIcon className="bckg-icon" icon={faTimes} />
-              </span>
-            </Card.Text>
+            {eleve.length !== 0 ? (
+              <div>
+                {eleve?.map((data) => (
+                  <Card.Text key={data.id}>
+                    {data.prenom} {data.nom}
+                    <span className="warning green">
+                      Mail d'avertissement déjà envoyé&nbsp;
+                      <FontAwesomeIcon className="bckg-icon" icon={faCheck} />
+                    </span>
+                    {data.rapport === 1 ? (
+                      <span className="warning green">
+                        Rapport déposé&nbsp;
+                        <FontAwesomeIcon className="bckg-icon" icon={faCheck} />
+                      </span>
+                    ) : (
+                      <span className="warning red">
+                        Rapport à télécharger&nbsp;
+                        <FontAwesomeIcon className="bckg-icon" icon={faTimes} />
+                      </span>
+                    )}
+                    {data.presentation === 1 ? (
+                      <span className="warning green">
+                        Présentation déposé<em></em>&nbsp;
+                        <FontAwesomeIcon className="bckg-icon" icon={faCheck} />
+                      </span>
+                    ) : (
+                      <span className="warning red">
+                        Présentation à télécharger&nbsp;
+                        <FontAwesomeIcon className="bckg-icon" icon={faTimes} />
+                      </span>
+                    )}
+                  </Card.Text>
+                ))}
+              </div>
+            ) : (
+              <Card.Text>Aucun élève n'est en retard !</Card.Text>
+            )}
           </Card.Body>
         </Card>
         <br />
